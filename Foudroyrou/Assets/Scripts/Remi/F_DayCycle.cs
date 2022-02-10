a55098b0de39a975e26244c17544affdff7ce73e
phoenix1267 <74063306+phoenix1267@users.noreply.github.com>
2022-02-10 10:12:37 +0100

    add my folder

---
+++ Foudroyrou/Assets/Scripts/Remi/F_DayCycle.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class F_DayCycle : MonoBehaviour
+{
+	#region f/p
+	#endregion
+
+	#region methods
+	#endregion
+}
---
+++ Foudroyrou/Assets/Scripts/Remi/F_World.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class F_World : MonoBehaviour
+{
+	#region f/p
+	#endregion
+
+	#region methods
+	#endregion
+}
