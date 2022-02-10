79c04490999dfb2e8a6fff74c88e9603f3fb8a9a
Nicolas DEROSES <nicolas2roz@gmail.com>
2022-02-10 16:01:27 +0100

    veins effect

--- Foudroyrou/Assets/Scripts/Nicolas/UI/F_RageUI.cs
+++ Foudroyrou/Assets/Scripts/Nicolas/UI/F_RageUI.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
+
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
 using System;
 
 public class F_RageUI : MonoBehaviour
@@ -8,16 +12,34 @@ public class F_RageUI : MonoBehaviour
 
     [SerializeField] Image rageBar = null;
 
+    [SerializeField] Volume volume = null;
+    Bloom bloom = null;
+
     public bool IsValid => rageBar;
+
+    public bool IsValidHDRP => volume;
 
     #endregion
 
     #region Methods
 
+    private void Start() => InitRageUI();
+
+    void InitRageUI()
+    {
+        if (!IsValidHDRP)
+            return;
+        VolumeProfile _profile = volume.sharedProfile;
+        _profile.TryGet<Bloom>(out bloom);            
+    }
+
     public void UpdateRageBar(float _amount)
     {
         if (!IsValid) return;
         rageBar.fillAmount = _amount;
+
+        if (!IsValidHDRP) return;
+        bloom.intensity.value = _amount;
     }
 
     #endregion
