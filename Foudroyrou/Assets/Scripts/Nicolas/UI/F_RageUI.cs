a66d96925c4bd4bae5fdc7f868325b145f487d43
Nicolas DEROSES <nicolas2roz@gmail.com>
2022-02-10 14:14:22 +0100

    UI : Rage/Time/Ammo

--- Foudroyrou/Assets/Scripts/Nicolas/UI/F_RageUI.cs
+++ Foudroyrou/Assets/Scripts/Nicolas/UI/F_RageUI.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class F_RageUI : MonoBehaviour
 {
     #region Fields&Properties
 
+    [SerializeField] Image rageBar = null;
 
+    public bool IsValid => rageBar;
 
     #endregion
 
     #region Methods
 
-
+    public void UpdateRageBar(float _amount)
+    {
+        if (!IsValid) return;
+        rageBar.fillAmount = _amount;
+    }
 
     #endregion
 }
--- Foudroyrou/Assets/Scripts/Nicolas/UI/F_TimeUI.cs
+++ Foudroyrou/Assets/Scripts/Nicolas/UI/F_TimeUI.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
+using TMPro;
 using System;
 
 public class F_TimeUI : MonoBehaviour
 {
     #region Fields&Properties
 
+    [SerializeField] TextMeshProUGUI timeUI = null;
 
+    public bool IsValid => timeUI;
 
     #endregion
 
     #region Methods
 
-
+    public void UpdateTimeText(string _time)
+    {
+        if (!IsValid) return;
+        timeUI.text = _time;
+    }
 
     #endregion
 }
