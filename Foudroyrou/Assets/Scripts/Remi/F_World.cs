be21a09d14d8bbef6e33aa9859bed9af97eaadf6
phoenix1267 <74063306+phoenix1267@users.noreply.github.com>
2022-02-10 12:04:19 +0100

    Add fx and wold

--- Foudroyrou/Assets/Scripts/Remi/F_World.cs
+++ Foudroyrou/Assets/Scripts/Remi/F_World.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class F_World : F_Singleton<F_World>
 {
-    
+    F_Player player = null;
+    public F_Player GetFirstPlayer => player;
+
+    /// <summary>
+    /// Add the player to the manager
+    /// </summary>
+    /// <param name="_player">the player to add</param>
+    /// <returns>true if succeed false otherwise</returns>
+    public bool AddPlayer(F_Player _player)
+    {
+        if (player)
+            return false;
+        player = _player;
+        return true;
+    }
 }
