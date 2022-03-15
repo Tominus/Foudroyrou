9ce5795f5501a5ea58a8d834b53f0abc25741403
Tominus <Tominusstarwars@gmail.com>
2022-03-15 16:20:19 +0100

    edsdfjk

--- SystemDeQuete/Assets/Scripts/Connect/SQ_BaseURL.cs
+++ SystemDeQuete/Assets/Scripts/Connect/SQ_BaseURL.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public static class SQ_BaseURL
 {
-    const string BASE_LINK = "http://localhost/quests/";
+    //const string BASE_LINK = "http://localhost/quests/";
+    const string BASE_LINK = "https://nassimpastissimulator.000webhostapp.com/Quests/";
     public static string QUEST_LINK = $"{BASE_LINK}getQuest.php";
 }
